2a75e91cd8c8c173a9964f4c8a0591696a6a55e4
raso <rasoulkarimi@live.com>
2025-07-04 22:35:38 +0330

    pass gen

--- parse/id.js
+++ parse/id.js
@@ -1,1 +1,1 @@
-import{customAlphabet as t}from"nanoid";const o=t("1234567890abcdefghABCDEFGH",8);export{o as getId};
+import{customAlphabet as t,nanoid as o}from"nanoid";const e=t("1234567890abcdefghABCDEFGH",8),s=()=>o();export{e as getId,s as getPass};
